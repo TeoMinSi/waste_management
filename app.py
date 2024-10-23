a46eef4dda4e2b04026b27e8badde1a85c2d748a
Minsi teo <teominsi@hotmail.com>
2024-10-23 15:42:05 +0800

    updated changes

--- app.py
+++ app.py
@@ -7,137 +7,19 @@ from streamlit_pills import pills
 import plotly.graph_objects as go
 import data_generation
 
+st.set_page_config(layout="wide")
 
 with st.sidebar:
     st.image("full_logo.png")
     st.title('Waste Management Dashboard')
-
-# st.title(":recycle: Waste Management Dashboard")
-
-col1, col2 = st.columns(2, vertical_alignment="center")
-with col1:
-    st.image("Waste-management.svg", width=300)
-with col2:
+    st.image("Waste-management.svg", width=250)
     st.write('''The Trash Management Dashboard is a digital platform designed to empower individuals, businesses, and government to monitor, manage, and reduce their waste effectively. 
-         
+
 By tracking trash levels in real-time and calculating waste emissions, the dashboard serves as a vital tool to help organizations work towards carbon-neutral goals.''')
 
 df_columns=["DateTime", "Main_Owner", "2nd_Owner","Bin_ID","Load(KG)","Full(Y/N)"]
 df = pd.read_csv('user1.csv', names=df_columns)
 df['DateTime']  = pd.to_datetime(df['DateTime'])
-
-st.subheader("Current Bin Capacity")
-
-#retrieving the latest row in data
-last_row = df.iloc[-1]
-current_load = last_row["Load(KG)"]
-
-#hardcoded max load
-max_load = 150
-
-percent_filled = (current_load / max_load)*100
-
-if current_load > 59.6:
-    st.markdown(
-        """
-        <style>
-        .button_selected{
-            display: inline-block;
-            outline: 0;
-            text-align: center;
-            border: 1px solid #babfc3;
-            padding: 8px;
-            min-height: 36px;
-            min-width: 36px;
-            color: #ffffff;
-            background: #142c1c;
-            border-radius: 4px;
-            font-weight: 500;
-            font-size: 14px;
-            box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 0px 0px;
-        }
-
-        .button_unselected{
-            display: inline-block;
-            outline: 0;
-            text-align: center;
-            border: 1px solid #babfc3;
-            padding: 8px;
-            min-height: 36px;
-            min-width: 36px;
-            color: #142c1c;
-            background: #e8f3f5;
-            border-radius: 4px;
-            font-weight: 500;
-            font-size: 14px;
-            box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 0px 0px;
-        }    
-                        
-        </style>             
-        <div>
-            <p class='button_selected'>Loaded</p>
-            <p class='button_unselected'>Unloaded</p>
-        </div>
-        """,
-        unsafe_allow_html=True,
-        )
-    
-else:
-    st.markdown(
-    """
-    <style>
-    .button_selected{
-        display: inline-block;
-        outline: 0;
-        text-align: center;
-        border: 1px solid #babfc3;
-        padding: 8px;
-        min-height: 36px;
-        min-width: 36px;
-        color: #ffffff;
-        background: #142c1c;
-        border-radius: 4px;
-        font-weight: 500;
-        font-size: 14px;
-        box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 0px 0px;
-    }
-
-    .button_unselected{
-        display: inline-block;
-        outline: 0;
-        text-align: center;
-        border: 1px solid #babfc3;
-        padding: 8px;
-        min-height: 36px;
-        min-width: 36px;
-        color: #142c1c;
-        background: #e8f3f5;
-        border-radius: 4px;
-        font-weight: 500;
-        font-size: 14px;
-        box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 0px 0px;
-    }    
-                    
-    </style>             
-    <div>
-        <p class='button_unselected'>Loaded</p>
-        <p class='button_selected'>Unloaded</p>
-    </div>
-    """,
-    unsafe_allow_html=True,
-)
-
-st.write(f"Bin Capacity as of {pd.to_datetime(last_row["DateTime"]).floor('S')} is {current_load}kg.")
-fig = go.Figure(go.Indicator(
-    mode = "gauge+number",
-        number = {'suffix':"%"},
-    gauge = {'bar': {'color': "#142c1c"},},
-    value = percent_filled,
-    domain = {'x': [0, 1], 'y': [0, 1]},
-    title = {'text': "Bin Capacity (Percentage)"}))
-    
-
-st.plotly_chart(fig)
 
 
 cleaned_df = df.drop(columns=['Main_Owner', '2nd_Owner',"Full(Y/N)"], axis=1)
@@ -148,7 +30,6 @@ fake_data_df['DateTime']  = pd.to_datetime(fake_data_df['DateTime'])
 
 joined_df = pd.concat([fake_data_df, cleaned_df], ignore_index=True, sort=False)
 
-
 realtime_df = joined_df.groupby(["Bin_ID", pd.Grouper(key='DateTime', axis=0, freq='1min')]).last().reset_index()
 hourly_df = realtime_df.groupby(["Bin_ID", pd.Grouper(key='DateTime', axis=0, freq='60min')]).last().reset_index()
 daily_df = realtime_df.groupby(["Bin_ID", pd.Grouper(key='DateTime', axis=0, freq='24h')]).last().reset_index()
@@ -158,22 +39,35 @@ monthly_df = realtime_df.groupby(["Bin_ID", pd.Grouper(key='DateTime', axis=0, f
 
 
 realtime_df.fillna(method='ffill', inplace=True)
-realtime_df = realtime_df.tail(60)
+realtime_df = realtime_df[realtime_df['DateTime'] > realtime_df['DateTime'].max() - pd.Timedelta(minutes=60)]
 
 hourly_df.fillna(method='ffill', inplace=True)
-hourly_df = hourly_df.tail(96)
+hourly_df = hourly_df[hourly_df['DateTime'] > hourly_df['DateTime'].max() - pd.Timedelta(hours=165)]
 
 daily_df.fillna(method='ffill', inplace=True)
-daily_df = daily_df.tail(30)
+daily_df = daily_df[daily_df['DateTime'] > daily_df['DateTime'].max() - pd.Timedelta(days=30)]
 
 weekly_df.fillna(method='ffill', inplace=True)
-weekly_df = weekly_df.tail(52)
+weekly_df = weekly_df[weekly_df['DateTime'] > weekly_df['DateTime'].max() - pd.Timedelta(weeks=52)]
 
 monthly_df.fillna(method='ffill', inplace=True)
-monthly_df = monthly_df.tail(24)
+monthly_df = monthly_df[monthly_df['DateTime'] > weekly_df['DateTime'].max() - pd.Timedelta(weeks=150)]
 
+def bin_labels(row):
+    if row["Bin_ID"] == "bin1":
+        row["Material"] = "Plastic"
+    if row["Bin_ID"] == "bin2":
+        row["Material"] = "Cardboard"
+    if row["Bin_ID"] == "bin3":
+        row["Material"] = "Metal"
+    return row
 
+#how to detect when the load is unloaded?
+monthly_df = monthly_df.apply(bin_labels, axis=1)
 
+monthly_df['ym-date'] = monthly_df['DateTime'].dt.strftime('%B-%Y')
+
+fig = px.histogram(monthly_df, x='ym-date', y='Load(KG)',color="Material",barmode='group',title='Breakdown of Recyclables')
 
 #charts
 
@@ -237,88 +131,210 @@ monthly_fig.add_hline(y=100, line_dash="dot",
               annotation_font_color="blue"
              )
 
+col1, col2 = st.columns(2)
 
-time_period = pills("Select your time period", ["Real-time", "Hourly", "Daily", "Weekly", "Monthly"], index=0)
-if time_period == "Real-time":
-    st.subheader("Real Time Chart")
-    tab1, tab2 = st.tabs(["Chart", "Data"])
-    with tab1:
-        st.write("Bin 1 is for General Waste while Bin 2 is for Recyclables.")
-        st.plotly_chart(realtime_fig, use_container_width=True)
-    with tab2:
-        st.dataframe(realtime_df, use_container_width =True)
-
-
-elif time_period == "Hourly":
-    st.subheader("Hourly Time Chart")
-    tab1, tab2 = st.tabs(["Chart", "Data"])
-    with tab1:
-        st.write("Bin 1 is for General Waste while Bin 2 is for Recyclables.")
-
-        st.plotly_chart(hourly_fig, use_container_width=True)
-    with tab2:
-        st.dataframe(hourly_df, use_container_width =True)
+with col1:
+    st.header("Waste Data")
+    time_period = pills("Select your time period", ["Real-time", "Hourly", "Daily", "Weekly", "Monthly"], index=0)
+    if time_period == "Real-time":
+        st.subheader("Real Time Chart")
+        tab1, tab2 = st.tabs(["Chart", "Data"])
+        with tab1:
+            st.write("Bin 1 is for General Waste while Bin 2 is for Recyclables.")
+            st.plotly_chart(realtime_fig, use_container_width=True)
+        with tab2:
+            st.dataframe(realtime_df, use_container_width =True)
 
 
-elif time_period == "Daily":
-    st.subheader("Daily Time Chart")
-    tab1, tab2 = st.tabs(["Chart", "Data"])
-    with tab1:
-        st.write("Bin 1 is for General Waste while Bin 2 is for Recyclables.")
+    elif time_period == "Hourly":
+        st.subheader("Hourly Time Chart")
+        tab1, tab2 = st.tabs(["Chart", "Data"])
+        with tab1:
+            st.write("Bin 1 is for General Waste while Bin 2 is for Recyclables.")
 
-        st.plotly_chart(daily_fig, use_container_width=True)
-    with tab2:
-        st.dataframe(daily_df, use_container_width =True)
-
-
-elif time_period == "Weekly":
-    st.subheader("Weekly Time Chart")
-    tab1, tab2 = st.tabs(["Chart", "Data"])
-    with tab1:
-        st.write("Bin 1 is for General Waste while Bin 2 is for Recyclables.")
-
-        st.plotly_chart(weekly_fig, use_container_width=True)
-    with tab2:
-        st.dataframe(weekly_df, use_container_width =True)
+            st.plotly_chart(hourly_fig, use_container_width=True)
+        with tab2:
+            st.dataframe(hourly_df, use_container_width =True)
 
 
-else:
-    st.subheader("Monthly Time Chart")
-    tab1, tab2 = st.tabs(["Chart", "Data"])
-    with tab1:
-        st.write("Bin 1 is for General Waste while Bin 2 is for Recyclables.")
+    elif time_period == "Daily":
+        st.subheader("Daily Time Chart")
+        tab1, tab2 = st.tabs(["Chart", "Data"])
+        with tab1:
+            st.write("Bin 1 is for General Waste while Bin 2 is for Recyclables.")
 
-        st.plotly_chart(monthly_fig, use_container_width=True)
-    with tab2:
-        st.dataframe(monthly_df, use_container_width =True)
+            st.plotly_chart(daily_fig, use_container_width=True)
+        with tab2:
+            st.dataframe(daily_df, use_container_width =True)
 
-st.divider()
 
-def activity_string(row):
-    main_user = row["Main_Owner"]
-    second_user = row["2nd_Owner"]
-    binID = row["Bin_ID"]
-    load = row["Load(KG)"]
-    isFull = row["Full(Y/N)"]
-    if isFull == "Y":
-        full_str = "full"
+    elif time_period == "Weekly":
+        st.subheader("Weekly Time Chart")
+        tab1, tab2 = st.tabs(["Chart", "Data"])
+        with tab1:
+            st.write("Bin 1 is for General Waste while Bin 2 is for Recyclables.")
+
+            st.plotly_chart(weekly_fig, use_container_width=True)
+        with tab2:
+            st.dataframe(weekly_df, use_container_width =True)
+
+
     else:
-        full_str= "not full"
-    
-    if load != 0:
-        activity =  f'{main_user} and {second_user} loaded into {binID}. Total weight of load is {load}kg. Bin is {full_str}.'
-    else:
-        activity = f'{main_user} and {second_user} unloaded {binID}. Total weight of load is {load}kg.'
-    return activity
+        st.subheader("Monthly Time Chart")
+        tab1, tab2 = st.tabs(["Chart", "Data"])
+        with tab1:
+            st.write("Bin 1 is for General Waste while Bin 2 is for Recyclables.")
 
-st.subheader("Activity Table")
-realtime_activity = df.groupby(pd.Grouper(key='DateTime', axis=0, freq='1min')).last().reset_index()
-cleaned_realtime_activity = realtime_activity.dropna(thresh=2)
-# st.write(cleaned_realtime_activity)
-cleaned_realtime_activity['activity'] = cleaned_realtime_activity.apply(activity_string, axis=1)
-cleaned_realtime_activity = cleaned_realtime_activity.drop(columns=['Main_Owner', '2nd_Owner',"Bin_ID", "Load(KG)" , "Full(Y/N)"], axis=1)
-re_indexed = cleaned_realtime_activity.reset_index(drop=True)
-st.dataframe(re_indexed, use_container_width =True)
+            st.plotly_chart(monthly_fig, use_container_width=True)
+        with tab2:
+            st.dataframe(monthly_df, use_container_width =True)
+
+    st.divider()
+
+    st.header("Collection Data")
+
+    #retrieving the latest row in data
+    last_row = df.iloc[-1]
+    current_load = last_row["Load(KG)"]
+
+    #hardcoded max load
+    max_load = 150
+
+    percent_filled = (current_load / max_load)*100
+
+    if current_load > 59.6:
+        st.markdown(
+            """
+            <style>
+            .button_selected{
+                display: inline-block;
+                outline: 0;
+                text-align: center;
+                border: 1px solid #babfc3;
+                padding: 8px;
+                min-height: 36px;
+                min-width: 36px;
+                color: #ffffff;
+                background: #142c1c;
+                border-radius: 4px;
+                font-weight: 500;
+                font-size: 14px;
+                box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 0px 0px;
+            }
+
+            .button_unselected{
+                display: inline-block;
+                outline: 0;
+                text-align: center;
+                border: 1px solid #babfc3;
+                padding: 8px;
+                min-height: 36px;
+                min-width: 36px;
+                color: #142c1c;
+                background: #e8f3f5;
+                border-radius: 4px;
+                font-weight: 500;
+                font-size: 14px;
+                box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 0px 0px;
+            }    
+                            
+            </style>             
+            <div>
+                <p class='button_selected'>Loaded</p>
+                <p class='button_unselected'>Unloaded</p>
+            </div>
+            """,
+            unsafe_allow_html=True,
+            )
+        
+    else:
+        st.markdown(
+        """
+        <style>
+        .button_selected{
+            display: inline-block;
+            outline: 0;
+            text-align: center;
+            border: 1px solid #babfc3;
+            padding: 8px;
+            min-height: 36px;
+            min-width: 36px;
+            color: #ffffff;
+            background: #142c1c;
+            border-radius: 4px;
+            font-weight: 500;
+            font-size: 14px;
+            box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 0px 0px;
+        }
+
+        .button_unselected{
+            display: inline-block;
+            outline: 0;
+            text-align: center;
+            border: 1px solid #babfc3;
+            padding: 8px;
+            min-height: 36px;
+            min-width: 36px;
+            color: #142c1c;
+            background: #e8f3f5;
+            border-radius: 4px;
+            font-weight: 500;
+            font-size: 14px;
+            box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 0px 0px;
+        }    
+                        
+        </style>             
+        <div>
+            <p class='button_unselected'>Loaded</p>
+            <p class='button_selected'>Unloaded</p>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    st.write(f"Bin Capacity as of {pd.to_datetime(last_row["DateTime"]).floor('S')} is {current_load}kg.")
+    gauge_fig = go.Figure(go.Indicator(
+        mode = "gauge+number",
+            number = {'suffix':"%"},
+        gauge = {'bar': {'color': "#142c1c"},},
+        value = percent_filled,
+        domain = {'x': [0, 1], 'y': [0, 1]},
+        title = {'text': "Bin Capacity (Percentage)"}))
+        
+
+    st.plotly_chart(gauge_fig)
+
+with col2:
+    def activity_string(row):
+        main_user = row["Main_Owner"]
+        second_user = row["2nd_Owner"]
+        binID = row["Bin_ID"]
+        load = row["Load(KG)"]
+        isFull = row["Full(Y/N)"]
+        if isFull == "Y":
+            full_str = "full"
+        else:
+            full_str= "not full"
+        
+        if load != 0:
+            activity =  f'{main_user} and {second_user} loaded into {binID}. Total weight of load is {load}kg. Bin is {full_str}.'
+        else:
+            activity = f'{main_user} and {second_user} unloaded {binID}. Total weight of load is {load}kg.'
+        return activity
+
+    st.header("Activity Table")
+    realtime_activity = df.groupby(pd.Grouper(key='DateTime', axis=0, freq='1min')).last().reset_index()
+    cleaned_realtime_activity = realtime_activity.dropna(thresh=2)
+    # st.write(cleaned_realtime_activity)
+    cleaned_realtime_activity['activity'] = cleaned_realtime_activity.apply(activity_string, axis=1)
+    cleaned_realtime_activity = cleaned_realtime_activity.drop(columns=['Main_Owner', '2nd_Owner',"Bin_ID", "Load(KG)" , "Full(Y/N)"], axis=1)
+    re_indexed = cleaned_realtime_activity.reset_index(drop=True)
+    st.dataframe(re_indexed, use_container_width =True)
+
+
+    st.divider()
+    #recyclables chart
+    st.plotly_chart(fig, use_container_width=True)
 
 _ ='''
 Questions:
--- data_generation.py
+++ data_generation.py
@@ -5,13 +5,18 @@ import numpy as np
 def data_generation():
     df_columns=["DateTime", "Main_Owner", "2nd_Owner","Bin_ID","Load(KG)","Full(Y/N)"]
     df = pd.read_csv('user1.csv', names=df_columns)
-    new_df = df[["DateTime"]]
+    bin2_df = df[["DateTime"]]
+    bin3_df = df[["DateTime"]]
 
-    new_df["Load(KG)"] = np.random.randint(0,100,size=(len(new_df)))
+    bin2_df["Load(KG)"] = np.random.randint(0,100,size=(len(bin2_df)))
+    bin3_df["Load(KG)"] = np.random.randint(0,100,size=(len(bin3_df)))
 
     users = ["user1","user2",'user3','user4']
     loaded = ["Y","N"]
 
-    new_df["Bin_ID"] = "bin2"
+    bin2_df["Bin_ID"] = "bin2"
+    bin3_df["Bin_ID"] = "bin3"
+
+    new_df = pd.concat([bin2_df,bin3_df], ignore_index=True, sort=False)
 
     return new_df
